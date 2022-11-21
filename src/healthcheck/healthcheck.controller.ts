09a3fc5ae915bb15c365563f1025dee2ff60a7a8
Aliaksandr <aliaksandr.budzevich@gmail.com>
2022-11-21 17:07:50 +0300

    WEB-109 swagger exclude healthCheck

--- src/healthcheck/healthcheck.controller.ts
+++ src/healthcheck/healthcheck.controller.ts
@@ -1,7 +1,9 @@
-import {Controller, Get} from '@nestjs/common';
-import {Paginate, PaginateQuery} from 'nestjs-paginate';
+import { Controller, Get } from '@nestjs/common';
+import { Paginate, PaginateQuery } from 'nestjs-paginate';
+import { ApiExcludeController } from '@nestjs/swagger';
 
 @Controller('healthcheck')
+@ApiExcludeController()
 export class HealthcheckController {
   @Get()
   findAll(@Paginate() query: PaginateQuery) {
