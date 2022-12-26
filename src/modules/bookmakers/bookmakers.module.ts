a3502fdba1582c7ae772d68db28b3d24df0c96c7
Aliaksandr Budzevich <aliaksandr.budzevich@gmail.com>
2022-12-26 17:59:02 +0300

    MVMW-28 group odds by bookmaker

--- src/modules/bookmakers/bookmakers.module.ts
+++ src/modules/bookmakers/bookmakers.module.ts
@@ -1,15 +1,13 @@
-import {Module} from '@nestjs/common';
-import {BookmakersService} from './bookmakers.service';
-import {BookmakersController} from './bookmakers.controller';
-import {bookmakerProviders} from "./bookmaker.providers";
-import {DatabaseModule} from "../../common/database/database.module";
+import { Module } from '@nestjs/common';
+import { BookmakersService } from './bookmakers.service';
+import { BookmakersController } from './bookmakers.controller';
+import { bookmakerProviders } from './bookmaker.providers';
+import { DatabaseModule } from '../../common/database/database.module';
 
 @Module({
-    imports: [DatabaseModule],
-    controllers: [BookmakersController],
-    providers: [
-        BookmakersService,
-      ...bookmakerProviders
-    ],
+  imports: [DatabaseModule],
+  controllers: [BookmakersController],
+  providers: [BookmakersService, ...bookmakerProviders],
+  exports: [...bookmakerProviders],
 })
 export class BookmakersModule {}
--- src/modules/odds/dto/odd-criteria.dto.ts
+++ src/modules/odds/dto/odd-criteria.dto.ts
@@ -2,10 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class OddCriteriaDto {
-  @IsString()
-  @ApiProperty()
-  @IsOptional()
-  match: string;
 
   @IsString()
   @ApiProperty()
